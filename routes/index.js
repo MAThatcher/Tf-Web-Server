977cb602362816ebcb14fd5d6e1646893b53e677
MAThatcher <58484785+MAThatcher@users.noreply.github.com>
2024-01-05 23:12:39 -0600

    .

--- routes/index.js
+++ routes/index.js
@@ -6,148 +6,125 @@ const path = require('path');
 const key = require('./key');
 
 router.get(`/message`, function (req, res, next) {
-  const { message, sender, character, location, radius } = req.query;
-  const content = `\`\`\`${sender}${sender !== character ? " (" + character + ")" : ""} ${radius}s:
+  try {
+    const { message, sender, character, location, radius } = req.query;
+    const content = `\`\`\`${sender}${sender !== character ? " (" + character + ")" : ""} ${radius}s:
 ${message}\`\`\``;
+    const content2 = content + `\`TeleportPlayer ${location}\``
 
-  axios.post(
-    `${key.chat}`,
-    {
-      content: content,
-    }
-  );
-  const content2 = content + `\`TeleportPlayer ${location}\``
-  axios.post(
-    `${key.home}`,
-    {
-      content: content2,
-    }
-  );
+    axios.post(
+      `${key.chat}`, { content: content }
+    );
 
-  res.json({
-    ManifestFileVersion: "000000000000",
-    bIsFileData: false,
-    AppID: "000000000000",
-    AppNameString: "",
-    BuildVersionString: "",
-    LaunchExeString: "",
-    LaunchCommand: "",
-    PrereqIds: [],
-    PrereqName: "",
-    PrereqPath: "",
-    PrereqArgs: "",
-    FileManifestList: [],
-    ChunkHashList: {},
-    ChunkShaList: {},
-    DataGroupList: {},
-    ChunkFilesizeList: {},
-    CustomFields: {},
-  });
+    axios.post(
+      `${key.home}`, { content: content2 }
+    );
+    res.json({
+      success: true,
+      message: '/message call successfull',
+    });
+  }
+  catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-
 router.get(`/log`, function (req, res, next) {
-  const { charName, eventType, eventId, eventCategory, params } = req.query;
+  try {
+    const { charName, eventType, eventId, eventCategory, params } = req.query;
 
-  const content = `\`\`\`Who:${charName}
-Log: ${eventType}
+    const content = `\`\`\`Who:${charName}
 Event Category:${eventCategory}
 Event Type:${eventType}
 Event Id: ${eventId}
 Params: ${params}\`\`\``;
-
-  axios.post(
-    `${key.rr}`,
-    {
-      content: content,
+    const shift = `${params}`
+    axios.post(
+      `${key.rr}`, { content: content, }
+    );
+    res.json({
+      success: true,
+      message: '/log call successfull',
+    });
+    if (eventType === 'Dice' && eventId === 'RR_ABILITY_USE' && eventCategory === 'Roleplay Redux') {
+      processShift(shift);
     }
-  );
-
-  res.json({
-    ManifestFileVersion: "000000000000",
-    bIsFileData: false,
-    AppID: "000000000000",
-    AppNameString: "",
-    BuildVersionString: "",
-    LaunchExeString: "",
-    LaunchCommand: "",
-    PrereqIds: [],
-    PrereqName: "",
-    PrereqPath: "",
-    PrereqArgs: "",
-    FileManifestList: [],
-    ChunkHashList: {},
-    ChunkShaList: {},
-    DataGroupList: {},
-    ChunkFilesizeList: {},
-    CustomFields: {},
-  });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.get(`/error`, function (req, res, next) {
-  const { message } = req.query;
+router.get(`/error`, function ({ query }, res, next) {
+  try {
+    const { message } = query;
+    const errormessage = message
 
-  const content = message
-
-  axios.post(
-    `${key.error}`,
-    {
-      content: content,
-    }
-  );
-
-  res.json({
-    ManifestFileVersion: "000000000000",
-    bIsFileData: false,
-    AppID: "000000000000",
-    AppNameString: "",
-    BuildVersionString: "",
-    LaunchExeString: "",
-    LaunchCommand: "",
-    PrereqIds: [],
-    PrereqName: "",
-    PrereqPath: "",
-    PrereqArgs: "",
-    FileManifestList: [],
-    ChunkHashList: {},
-    ChunkShaList: {},
-    DataGroupList: {},
-    ChunkFilesizeList: {},
-    CustomFields: {},
-  });
+    axios.post(
+      `${key.error}`,
+      {
+        content: errormessage
+      });
+    res.json({
+      success: true,
+      message: 'Error message received and logged.',
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 router.get(`/personal`, function (req, res, next) {
-  const { message, sender} = req.query;
-  const content = `\`\`\`${sender}:${message}\`\`\``;
+  try {
+    const { message, sender } = req.query;
+    const content = `\`\`\`${sender}:${message}\`\`\``;
 
-  axios.post(
-    `${key.personal}`,
-    {
-      content: content,
-    }
-  );
-
-  res.json({
-    ManifestFileVersion: "000000000000",
-    bIsFileData: false,
-    AppID: "000000000000",
-    AppNameString: "",
-    BuildVersionString: "",
-    LaunchExeString: "",
-    LaunchCommand: "",
-    PrereqIds: [],
-    PrereqName: "",
-    PrereqPath: "",
-    PrereqArgs: "",
-    FileManifestList: [],
-    ChunkHashList: {},
-    ChunkShaList: {},
-    DataGroupList: {},
-    ChunkFilesizeList: {},
-    CustomFields: {},
-  });
+    axios.post(
+      `${key.personal}`,
+      {
+        content: content,
+      }
+    );
+    res.json({
+      success: true,
+      message: '/personal call successfull',
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-module.exports = router;
+function processShift(shift) {
+  const shiftTypes = {
+    "Start Lowtown Tavern Shift": "Lowtown Tavern",
+    "Start Hightown Tavern Shift": "Hightown Tavern",
+    "Start Lowtown Brothel Shift": "Lowtown Brothel",
+    "Start Hightown Brothel Shift": "Hightown Brothel"
+  };
 
+  for (const [shiftType, location] of Object.entries(shiftTypes)) {
+    if (shift.includes(shiftType)) {
+      const pipeIndex = shift.indexOf('|');
+      const openParenIndex = shift.indexOf('(');
+      const nameSubstring = shift.substring(pipeIndex + 1, openParenIndex).trim();
+      const content2 = `:exclamation: ${nameSubstring} has started working at the ${location}!`;
+      postShift(content2, key.rr);
+      break;
+    }
+  }
+}
+function postShift(content, key) {
+  try {
+    axios.post(
+      key, { content }
+    );
+  }
+  catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+module.exports = router;
