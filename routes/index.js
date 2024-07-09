89dd42a41eb9867c1278f7a13f4f566d2b425f90
MAThatcher <58484785+MAThatcher@users.noreply.github.com>
2024-07-09 12:45:22 -0500

    .
    .

--- routes/index.js
+++ routes/index.js
@@ -50,14 +50,16 @@ Params: ${params}\`\`\``;
     if (eventType === 'Dice' && eventId === 'RR_ABILITY_USE' && eventCategory === 'Roleplay Redux') {
       processShift(shift);
     }
-    if (eventType === 'FlowChart' && eventId === 'FlowChartLog' && eventCategory === 'Admin') {
-      axios.post(
-        `${key.server}`, { content: ":white_check_mark: Server is Online", }
-      );
-      res.json({
-        success: true,
-        message: '/log call successfull: Server is online',
-      });
+    if (eventType === 'FlowChart' && eventId === 'FlowChartLog' && eventCategory === 'Admin' && params.includes('The server has started!')) {
+      try {
+        serverOnline = ':white_check_mark: Server is Online ~Ignore this test~'
+        axios.post(
+          `${key.server}`, { content: serverOnline }
+        );
+      }
+      catch (error) {
+        console.error("Error:", error.message);
+      }
     }
   } catch (error) {
     console.error("Error:", error.message);
@@ -141,4 +143,47 @@ function postShift(content, key) {
   }
 }
 
+router.get(`/RenMessage`, function (req, res, next) {
+  try {
+    const { message, sender, character, location, radius } = req.query;
+    const content = `\`\`\`${sender}${sender !== character ? " (" + character + ")" : ""} ${radius}s:
+${message}\`\`\``;
+
+    axios.post(
+      `${key.RenMessage}`, { content: content }
+    );
+
+    res.json({
+      success: true,
+      message: '/message call successfull',
+    });
+  }
+  catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get(`/RenLog`, function (req, res, next) {
+  try {
+    const { charName, eventType, eventId, eventCategory, params } = req.query;
+
+    const content = `\`\`\`Who:${charName}
+Event Category:${eventCategory}
+Event Type:${eventType}
+Event Id: ${eventId}
+Params: ${params}\`\`\``;
+    const shift = `${params}`
+    axios.post(
+      `${key.RenLog}`, { content: content, }
+    );
+    res.json({
+      success: true,
+      message: '/log call successfull',
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 module.exports = router;
